59e93dd62951b731195d8f8b1a53868d669aba31
Aman <amanmaurya8419@gmail.com>
2023-07-07 13:39:54 +0530

    updated portfolio

--- README.md
+++
@@ -1,1 +0,0 @@
-# Amanmauryacode.github.io
--- index.js
+++ index.js
@@ -1,47 +1,95 @@
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontains = document.getElementsByClassName("tab-contains");
-
-
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-    }
-    for(tabcontain of tabcontains){
-        tabcontain.classList.remove("active-tab");
-    }
-
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-}
-
-var sidemenu = document.getElementById("sidemenu");
-
-function openmenu(){
-    sidemenu.style.right = "0";
-}
-
-function closemenu(){
-    sidemenu.style.right = "-200px";
-}
-
-
-
-const showMoreButton = document.getElementById('show-more-button');
-const projects = document.querySelectorAll('.work');
-
-let visibleProjects = 3;
-
-showMoreButton.addEventListener('click', function() {
-  visibleProjects += 3;
-  for (let i = 0; i < projects.length; i++) {
-    if (i < visibleProjects) {
-      projects[i].classList.add('show-project');
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $("#nav-menu").addClass("sticky");
     } else {
-      projects[i].classList.remove('show-project');
+      $("#nav-menu").removeClass("sticky");
     }
-  }
-  
-  if (visibleProjects >= projects.length) {
-    showMoreButton.style.display = 'none';
-  }
+
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
+
+  $("#nav-menu .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
+
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $("#nav-menu .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: ["Full Stack Developer", "Daydreamer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  var typed = new Typed(".typing-2", {
+    strings: ["Full Stack Developer", "Daydreamer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
 });
+
+let drive = document
+  .querySelector(".Drivegoogle")
+  .addEventListener("click", openDrive);
+
+function openDrive() {
+  window.open(
+    (href =
+      "https://drive.google.com/file/d/1Dhg4ILRmXMtSDuTJwAWcMUiWQ5cifVpC/view?usp=sharing")
+  );
+}
+
+let drive2 = document
+  .querySelector(".googleDrive")
+  .addEventListener("click", openDrive2);
+
+function openDrive2() {
+  window.open(
+    (href =
+      "https://drive.google.com/file/d/1Dhg4ILRmXMtSDuTJwAWcMUiWQ5cifVpC/view?usp=sharing")
+  );
+}
